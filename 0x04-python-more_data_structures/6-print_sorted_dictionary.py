41dc2aa7fff24651964dec4c3ae9ee9bf7b069f9
mugane <muganes25@gmail.com>
2023-01-11 03:22:44 -0800

    sorted dict

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    "Print a sorted dictionary"
+
+    for key, value in a_dictionary.items():
+        print("{}: {}".format(key, value))
