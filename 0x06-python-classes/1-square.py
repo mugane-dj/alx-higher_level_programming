276792451956e9c205e9b71de72ffeb0869a8307
mugane <muganes25@gmail.com>
2023-01-24 03:37:45 -0800

    init

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Defines a class Square"""
+
+
+class Square:
+    """Definition of the square"""
+
+    def __init__(self, size):
+        """Defines instance variables"""
+
+        self.__size = size
