b2265babdbd7c467e3e6e2218fb6c9a472b52989
mugane <muganes25@gmail.com>
2023-01-24 03:45:12 -0800

    exceptions

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Defines a class Square"""
+
+
+class Square:
+    """Definition of the square"""
+
+    def __init__(self, size=0):
+        """Defines instance variables"""
+
+        self.__size = size
+
+        if (type(size) != int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            pass
