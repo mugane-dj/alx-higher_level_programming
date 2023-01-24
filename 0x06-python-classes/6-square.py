cfaaab19d4b97400891966afd05d935094678cef
mugane <muganes25@gmail.com>
2023-01-24 04:41:37 -0800

    pos

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+"""Defines a class Square"""
+
+
+class Square:
+    """Definition of the square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Defines instance variables"""
+
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Getter function"""
+
+        return self.__size
+
+    @property
+    def position(self):
+        """Getter function"""
+
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        """Setter function"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        """Setter function"""
+        if (not isinstance(value, tuple) or
+                not all(isinstance(i, int) for i in value) or
+                not all(i >= 0 for i in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Calculates the area of the square"""
+
+        area = self.__size ** 2
+        return area
+
+    def my_print(self):
+        """Prints the square to stdout"""
+        for i in range(0, self.__size):
+            for j in range(0, self.__position[0]):
+                for k in range(0, self.__size):
+                    print("#", end="")
+                print(" ", end="")
+            print("")
+        if self.__size == 0:
+            print("")
