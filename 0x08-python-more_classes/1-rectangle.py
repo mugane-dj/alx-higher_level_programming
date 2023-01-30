c25802d7cfa85dd8b4ca8644d709045b1eef744f
mugane <muganes25@gmail.com>
2023-01-30 02:14:37 -0800

    0x01

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -27,3 +27,20 @@ class Rectangle:
             raise ValueError("width must be >= 0")
 
         self.__width = value
+
+    @property
+    def height(self):
+        """Getter function"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter function for height attribute"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
