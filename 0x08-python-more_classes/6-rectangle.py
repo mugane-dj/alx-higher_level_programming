cc35ef4d9fc668624330265a3737c52fd491ccac
mugane <muganes25@gmail.com>
2023-01-30 03:06:23 -0800

    0x06

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -12,7 +12,7 @@ class Rectangle:
 
         self.width = width
         self.height = height
-        number_of_instances += 1
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
@@ -71,5 +71,5 @@ class Rectangle:
 
     def __del__(self):
         """Deletes an instance of Rectangle class"""
-        number_of_instances -= 1
+        type(self).number_of_instances -= 1
         print("Bye rectangle...")
