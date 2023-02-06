995596073929af7e3e7dd8cf155bec882b00afd9
mugane <muganes25@gmail.com>
2023-02-05 23:58:48 -0800

    0x02

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,6 +3,17 @@
 """Defines is_same_class module"""
 
 def is_same_class(obj, a_class):
-    """Returns bool if obj is an instance of a_class"""
+    """Returns bool if obj is an instance of a_class
 
-    return isinstance(obj, a_class)
+    Args:
+        obj (any): Object to check.
+        a_class (type): class to check obj against.
+    Returns:
+        True is obj is an instance of a_class
+        False otherwise
+    """
+
+
+    if type(obj) == a_class:
+        return True
+    return False
