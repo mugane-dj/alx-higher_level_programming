43b9e95b05543d0595a8acb794eacdc0f549d78f
mugane <muganes25@gmail.com>
2023-02-07 01:31:34 -0800

    0x00

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -10,6 +10,6 @@ def read_file(filename=""):
         filename (.txt): text file to read.
     """
 
-    with open(filename, encoding='utf-8') as f:
+    with open(filename, encoding="utf-8") as f:
         read_data = f.read()
-        print(read_data)
+        print(read_data, end="")
