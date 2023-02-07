f7b08cd4cccefe602fedeaf2cba2509c8d649864
mugane <muganes25@gmail.com>
2023-02-07 01:29:18 -0800

    0x00

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defines the read file module"""
+
+
+def read_file(filename=""):
+    """Reads a text file and prints it to stdout
+
+    Args:
+        filename (.txt): text file to read.
+    """
+
+    with open(filename, encoding='utf-8') as f:
+        read_data = f.read()
+        print(read_data)
