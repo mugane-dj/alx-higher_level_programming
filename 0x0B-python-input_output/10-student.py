15f445acec903b0e202fca23c7ffa8d6eb2a4729
mugane <muganes25@gmail.com>
2023-02-07 04:18:16 -0800

    0xA

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+"""Defines the Student class"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializer for Student class"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary representation of an instance of Student"""
+        if (type(attrs) == list and
+                all(type(attr) == str for attr in attrs)):
+            for attr in attrs:
+                if hasattr(self, attr):
+                    return {attr: getattr(self, attr)}
+        return self.__dict__
