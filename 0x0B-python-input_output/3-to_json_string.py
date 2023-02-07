278314a000780a7bb9d20b38d7440800646baeb9
mugane <muganes25@gmail.com>
2023-02-07 02:31:49 -0800

    0x03

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Defines the string to JSON module"""
+
+import json
+
+def to_json_string(my_obj):
+    """Converts string to JSON
+
+    Args:
+        obj(str): string to convert to JSON.
+    Returns:
+        JSON representation
+    """
+
+    return json.dumps(my_obj)
