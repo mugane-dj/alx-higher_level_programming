a649c0e257f6ae9c52e538c105849855cde272de
mugane <muganes25@gmail.com>
2023-02-12 22:25:08 -0800

    0xF

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 """Defines the Base class module"""
+import json
 
 
 class Base:
@@ -15,3 +16,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON String representation of list_dictionaries"""
+
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
