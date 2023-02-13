649cdf290690db0331577513b20898e7c5397c00
mugane <muganes25@gmail.com>
2023-02-12 22:41:08 -0800

    0x11

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -39,3 +39,12 @@ class Base:
         filename = cls.__name__ + ".json"
         with open(filename, 'w', encoding="utf-8") as f:
             f.write(data)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation json_string"""
+
+        if json_string is None:
+            return "[]"
+        else:
+            return json.loads(json_string)
