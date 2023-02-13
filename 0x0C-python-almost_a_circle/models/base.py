0aa70eefbe32b52d7d7e339f1b9fd717e182e570
mugane <muganes25@gmail.com>
2023-02-12 22:35:32 -0800

    0x10

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,3 +25,17 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+    
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file"""
+
+        if list_objs is None:
+            data = "[]"
+        else:
+            list_dicts = [obj.to_dictionary() for obj in list_objs]
+            data = Base.to_json_string(list_dicts)
+
+        filename = cls.__name__ + ".json"
+        with open(filename, 'w', encoding="utf-8") as f:
+            f.write(data)
