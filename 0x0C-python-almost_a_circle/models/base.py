b3647870fff8759fe06a73ab6fe6e40dc05b764b
mugane <muganes25@gmail.com>
2023-02-13 08:44:21 -0800

    0x01

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,7 +25,7 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
-    
+
     @classmethod
     def save_to_file(cls, list_objs):
         """Writes the JSON string representation of list_objs to a file"""
