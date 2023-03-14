611b2e93983c8f9c071bd9235247383c93ced339
mugane <muganes25@gmail.com>
2023-03-14 10:30:15 -0700

    0x10

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,3 @@
 -- List all records with name value.
-SELECT score, name FROM second_table WHERE name IS NOT NULL;
+SELECT score, name FROM second_table 
+WHERE name IS NOT NULL ORDER BY score DESC;
