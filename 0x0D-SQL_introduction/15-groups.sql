ca03becb1127ac5f4f2076fdc6f55f58b149a107
mugane <muganes25@gmail.com>
2023-03-14 10:21:20 -0700

    0xF

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+-- Lists the number of records with the same score in the table.
+SELECT score, COUNT(score) as number FROM second_table 
+GROUP BY score HAVING COUNT(score) > 1;
+ALTER TABLE second_table ADD number int;
+SELECT score, number FROM second_table ORDER BY number DESC;
