5fa43bc3e214b13c0f7ca796765c8163a788d1cd
mugane <muganes25@gmail.com>
2023-03-15 09:28:34 -0700

    0xF

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,10 @@
+-- List all Comedy shows in the database hbtn_0d_tvshows.
+SELECT a.title
+  FROM tv_shows AS a
+       INNER JOIN tv_show_genres AS b
+       ON a.id = b.show_id
+
+       INNER JOIN tv_genres AS c
+       ON c.id = b.genre_id
+       WHERE c.name = "Comedy"
+ORDER BY a.title
