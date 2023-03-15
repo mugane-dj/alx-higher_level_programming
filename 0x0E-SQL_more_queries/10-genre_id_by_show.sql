a9ab7af982a82735b6f3d8e40b2a01cfbf84f809
mugane <muganes25@gmail.com>
2023-03-15 07:59:46 -0700

    0xA

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 SELECT a.title, b.genre_id
   FROM tv_shows AS a
-       INNER JOIN tv_show_genres as b
+       INNER JOIN tv_show_genres AS b
        ON a.id = b.show_id
 ORDER BY a.title, b.genre_id; 
