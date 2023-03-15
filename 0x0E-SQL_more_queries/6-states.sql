72de54688a3d312f894095ab86623b0b01b0daa0
mugane <muganes25@gmail.com>
2023-03-15 05:46:13 -0700

    0x6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,6 @@
+-- Creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+    id int PRIMARY_KEY,
+    name varchar(256) NOT NULL
+);
