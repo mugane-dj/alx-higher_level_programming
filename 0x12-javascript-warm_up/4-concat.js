59c89f6631ef41d431503ea519ebdf10697d5823
mugane <muganes25@gmail.com>
2023-04-11 02:41:00 -0700

    0x4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-const process = require('node:process');
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
-if (arg1 && arg2) {
-  console.log(arg1 + ' is ' + arg2);
-} else if (arg1 && !arg2) {
-  console.log(arg1 + ' is ' + arg2);
+if (process.argv[2] && process.argv[3]) {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
+} else if (process.argv[2] && !process.argv[3]) {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
 } else {
-  console.log(arg1 + ' is ' + arg2);
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
 }
