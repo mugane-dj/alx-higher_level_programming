ca5e299d9e804d35ffb8767b4355ec6dd68e7ca0
mugane <muganes25@gmail.com>
2023-04-11 02:45:07 -0700

    0x7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-const process = require('node:process');
-const arg = process.argv[2];
-if (parseInt(arg)) {
-  for (let i = 0; i < parseInt(arg); i++) {
+if (parseInt(process.argv[2])) {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
     console.log('C is fun');
   }
 } else {
