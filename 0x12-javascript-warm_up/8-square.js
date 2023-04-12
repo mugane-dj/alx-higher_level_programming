75737325ffa20ec9b86fde637083e425be509115
Samuel Mugane <muganes25@gmail.com>
2023-04-12 15:28:24 +0300

    0x8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (parseInt(process.argv[2])) {
+if (process.argv[2]) {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
     let pattern = '';
     for (let j = 0; j < parseInt(process.argv[2]); j++) {
