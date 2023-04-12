9c200acb0bc389d14ce7ef657d6afa98a033a689
mugane <muganes25@gmail.com>
2023-04-12 05:32:03 -0700

    0x8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ if (process.argv[2]) {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
     let pattern = '';
     for (let j = 0; j < parseInt(process.argv[2]); j++) {
-      pattern += 'x';
+      pattern += 'X';
     }
     console.log(pattern);
   }
