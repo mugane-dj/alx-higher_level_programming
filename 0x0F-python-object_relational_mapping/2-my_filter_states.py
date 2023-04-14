b31e69f711713d8980410474301e137937cda806
mugane <muganes25@gmail.com>
2023-04-14 07:08:47 -0700

    0x2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,19 +3,20 @@
 hbtn_0e_0_usa"""
 import MySQLdb
 import sys
-username = sys.argv[1]
-password = sys.argv[2]
-db_name = sys.argv[3]
-state_name = sys.argv[4]
-db = MySQLdb.connect(host='localhost', port=3306, user=username,
-                     password=password, database=db_name)
-cur = db.cursor()
-query = """
-    SELECT id, name FROM states WHERE states.name='%s'
-    """ % (state_name)
-cur.execute(query)
-states = cur.fetchall()
-for state in states:
-    print(state)
-cur.close()
-db.close()
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+    db = MySQLdb.connect(host='localhost', port=3306, user=username,
+                         password=password, database=db_name)
+    cur = db.cursor()
+    query = ("SELECT id, name FROM states WHERE states.name='{}'"
+             "ORDER BY states.id ASC".format(state_name))
+    cur.execute(query)
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
