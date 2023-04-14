a75239113ef619151c96ee4a1648aef488f752b2
mugane <muganes25@gmail.com>
2023-04-14 07:15:06 -0700

    0x1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""This module contains a script that lists all states from the database
-hbtn_0e_0_usa"""
+"""This module contains a script that lists all states that start with letter N
+from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
