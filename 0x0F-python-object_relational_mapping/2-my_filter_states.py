d10d53e97941a8cfc170dd1cb94e3641d5c2047c
mugane <muganes25@gmail.com>
2023-04-14 06:53:33 -0700

    0x2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module contains a script that lists all states from the database
+hbtn_0e_0_usa"""
+import MySQLdb
+import sys
+username = sys.argv[1]
+password = sys.argv[2]
+db_name = sys.argv[3]
+state_name = sys.argv[4]
+db = MySQLdb.connect(host='localhost', port=3306, user=username,
+                     password=password, database=db_name)
+cur = db.cursor()
+query = """
+    SELECT id, name FROM states WHERE states.name='%s'
+    """ % (state_name)
+cur.execute(query)
+states = cur.fetchall()
+for state in states:
+    print(state)
+cur.close()
+db.close()
