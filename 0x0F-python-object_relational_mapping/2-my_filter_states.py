514c311945fcd45a727e0f504e6215e5da90f13d
mugane <muganes25@gmail.com>
2023-04-14 07:16:48 -0700

    0x2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""This module contains a script that lists all states from the database
-hbtn_0e_0_usa"""
+"""This module contains a script that lists the state that matches
+the state passed as argument from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
