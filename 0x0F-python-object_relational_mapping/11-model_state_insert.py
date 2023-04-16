1c97be4727f5b8c3ab7605ee827f4c9548291b9a
mugane <muganes25@gmail.com>
2023-04-16 02:03:42 -0700

    0xB

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module contains a script that adds
+State object to the database hbtn_0e_6_usa
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           user, password, db_name), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.add(State(name='Louisiana'))
+    session.commit()
+    instance = session.query(State).filter_by(name='Louisiana').first()
+    print(instance.id)
+    session.close()
