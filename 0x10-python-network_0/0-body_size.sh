59e5a39e73583052d8cd90b606807310ad3906f7
mugane <muganes25@gmail.com>
2023-04-27 06:35:04 -0700

    0x0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#This script displays the size of the body of the response
+#This script displays the size of the body of a curl response
 curl -sI "$1" | grep -i 'content-length' | awk '{print $2}'
