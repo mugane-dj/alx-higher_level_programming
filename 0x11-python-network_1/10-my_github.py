82c0bd342fc651f3fb117f88b76887af04bc74cd
mugane <muganes25@gmail.com>
2023-04-28 05:28:51 -0700

    0x9

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,15 +2,13 @@
 """This script fetches the id of a github user"""
 import requests
 import sys
+from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    url = "https://api.github.com/users/{}".format(username)
-    headers = {
-        'Authorization': 'Basic ' + password
-    }
-
-    res = requests.get(url=url, headers=headers).json()
+    url = "https://api.github.com/user"
+    auth = HTTPBasicAuth(username, password)
+    res = requests.get(url=url, auth=auth).json()
     print(res.get('id'))
