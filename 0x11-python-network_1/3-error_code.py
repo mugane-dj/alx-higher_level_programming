c4a75e0d69e01abeab401fe98e1339c31d654b8d
mugane <muganes25@gmail.com>
2023-04-28 04:10:11 -0700

    0x3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This script fetches headers from a url using urllib"""
+import urllib.request
+import urllib.error
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with urllib.request.urlopen(url) as f:
+            print(f.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: %d".format(e.status))
