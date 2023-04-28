35b7192988e9031a3eea0e987256d620889e5c20
mugane <muganes25@gmail.com>
2023-04-28 04:33:33 -0700

    0x6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     data = {"email": email}
-    res = requests.post(url, data=date)
+    res = requests.post(url, data=data)
     print(res.text)
