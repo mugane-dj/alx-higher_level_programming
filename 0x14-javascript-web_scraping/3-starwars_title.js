1a7921ab3db5fb23474a703d5f9035308ba51eae
mugane-dj <muganes25@gmail.com>
2023-05-23 07:50:47 +0000

    0x3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,6 +5,8 @@ const id = process.argv[2];
 const baseURL = 'https://swapi-api.alx-tools.com/api/films/:id';
 const url = baseURL.replace(':id', id);
 request(url, function (error, response, body) {
-  console.log(error);
+  if (error) {
+    throw new Error(error);
+  }
   console.log(JSON.parse(body).title);
 });
